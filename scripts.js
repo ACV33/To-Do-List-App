7710366501b8c79617d36dcfab842adf6ec11f21
ACV33 <ashli.vaccaro@gmail.com>
2022-05-28 13:56:57 -0400

    add to do list click function
    used jQuery to create a function that would allow users to click add and make a list of items. Items can be crossed out with a double click

---
+++ scripts.js
@@ -0,0 +1,33 @@
+
+function newItem() {
+
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if(inputValue === '') {
+      alert("Please submit an item to add to the list!");
+    } else {
+      $('#list').append(li);
+    }
+
+  function crossOut(){
+    li.toggleClass("strike");
+  }
+
+  li.on("onclick", function crossOut() {
+    li.toggleClass("strike");
+
+});
+
+    let crossOutButton = $("<crossOutButton></crossOutButton>");
+    crossOutButton.append(document.createTextNode("x"));
+    li.append(crossOutButton);
+
+    crossOutButton.on("click", deleteListItem);
+    function deleteListItem() {
+      li.addClass("delete");
+    }
+
+    $('#list').sortable();
+}
